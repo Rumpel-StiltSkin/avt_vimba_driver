593fac3654754e1a5fc63d215fcf57ec5b64e19c
Rumepl-StiltSkin <samuel_ong94@hotmail.com>
2017-12-06 18:18:34 +0800

    added RosVimbaApi

--- src/camera.cpp
+++ src/camera.cpp
@@ -7,74 +7,6 @@
 
 using AVT::VmbAPI::FramePtr;
 
-bool frameToImage(const FramePtr pFrame, sensor_msgs::Image& image) {
-      VmbPixelFormatType pixel_format;
-      VmbUint32_t width, height, nSize;
-    
-      pFrame->GetWidth(width);
-      pFrame->GetHeight(height);
-      pFrame->GetPixelFormat(pixel_format);
-      pFrame->GetImageSize(nSize);
-    
-      VmbUint32_t step = nSize / height;
-    
-      // NOTE: YUV and ARGB formats not supported
-      std::string encoding;
-      if      (pixel_format == VmbPixelFormatMono8          ) encoding = sensor_msgs::image_encodings::MONO8;
-      else if (pixel_format == VmbPixelFormatMono10         ) encoding = sensor_msgs::image_encodings::MONO16;
-      else if (pixel_format == VmbPixelFormatMono12         ) encoding = sensor_msgs::image_encodings::MONO16;
-      else if (pixel_format == VmbPixelFormatMono12Packed   ) encoding = sensor_msgs::image_encodings::MONO16;
-      else if (pixel_format == VmbPixelFormatMono14         ) encoding = sensor_msgs::image_encodings::MONO16;
-      else if (pixel_format == VmbPixelFormatMono16         ) encoding = sensor_msgs::image_encodings::MONO16;
-      else if (pixel_format == VmbPixelFormatBayerGR8       ) encoding = sensor_msgs::image_encodings::BAYER_GRBG8;
-      else if (pixel_format == VmbPixelFormatBayerRG8       ) encoding = sensor_msgs::image_encodings::BAYER_RGGB8;
-      else if (pixel_format == VmbPixelFormatBayerGB8       ) encoding = sensor_msgs::image_encodings::BAYER_GBRG8;
-      else if (pixel_format == VmbPixelFormatBayerBG8       ) encoding = sensor_msgs::image_encodings::BAYER_BGGR8;
-      else if (pixel_format == VmbPixelFormatBayerGR10      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerRG10      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerGB10      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerBG10      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerGR12      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerRG12      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerGB12      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerBG12      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerGR12Packed) encoding = sensor_msgs::image_encodings::TYPE_32SC4;
-      else if (pixel_format == VmbPixelFormatBayerRG12Packed) encoding = sensor_msgs::image_encodings::TYPE_32SC4;
-      else if (pixel_format == VmbPixelFormatBayerGB12Packed) encoding = sensor_msgs::image_encodings::TYPE_32SC4;
-      else if (pixel_format == VmbPixelFormatBayerBG12Packed) encoding = sensor_msgs::image_encodings::TYPE_32SC4;
-      else if (pixel_format == VmbPixelFormatBayerGR16      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerRG16      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerGB16      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatBayerBG16      ) encoding = sensor_msgs::image_encodings::TYPE_16SC1;
-      else if (pixel_format == VmbPixelFormatRgb8           ) encoding = sensor_msgs::image_encodings::RGB8;
-      else if (pixel_format == VmbPixelFormatBgr8           ) encoding = sensor_msgs::image_encodings::BGR8;
-      else if (pixel_format == VmbPixelFormatRgba8          ) encoding = sensor_msgs::image_encodings::RGBA8;
-      else if (pixel_format == VmbPixelFormatBgra8          ) encoding = sensor_msgs::image_encodings::BGRA8;
-      else if (pixel_format == VmbPixelFormatRgb12          ) encoding = sensor_msgs::image_encodings::TYPE_16UC3;
-      else if (pixel_format == VmbPixelFormatRgb16          ) encoding = sensor_msgs::image_encodings::TYPE_16UC3;
-      else
-        ROS_WARN("Received frame with unsupported pixel format %d", pixel_format);
-      if (encoding == "") return false;
-    
-      VmbUchar_t *buffer_ptr;
-      VmbErrorType err = pFrame->GetImage(buffer_ptr);
-      bool res = false;
-      if ( VmbErrorSuccess == err ) {
-        res = sensor_msgs::fillImage(image,
-                                     encoding,
-                                     height,
-                                     width,
-                                     step,
-                                     buffer_ptr);
-      } else {
-        ROS_ERROR_STREAM("[" << ros::this_node::getName()
-          << "]: Could not GetImage. ");
-      }
-      return res;
-}
-
-
-
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "image_publisher");
@@ -94,7 +26,7 @@ int main(int argc, char** argv)
   VmbErrorType err = VmbErrorSuccess;
 
   AVT::VmbAPI::Examples::ApiController apiController;
-  //ROS::VmbAPI::RosVimbaApi rosVimbaApi;  
+  ROS::VmbAPI::RosVimbaApi rosVimbaApi;  
 
   // Startup Vimba
   err = apiController.StartUp();        
@@ -142,7 +74,7 @@ int main(int argc, char** argv)
         //  std::cout < "error\n";
         //} 
 
-        if (frameToImage(pFrame, img))
+        if (rosVimbaApi.frameToImage(pFrame, img))
         {
           //sensor_msgs::CameraInfo ci = pFrame->getCameraInfo();
           //ci.header.stamp = img.header.stamp = ros_time;
